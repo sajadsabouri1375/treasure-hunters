Distance calculation bug fixed. Chasing player when not in sight as far as the last position in sight is in sight solved. This might need subtle modification to chase after hunter even when last position in sight is not visible. Some minor modification and debugging in vector utils which fixed infinity distance calculation.

hunters.py
@@ -10,13 +10,9 @@ class Hunter(IntelligentPlayer):
 
         self._is_treasure_hunted = False
         self._is_hunter_arrested = False
-        self._last_position_in_sight = None
         
     def update_protector_status(self, protector):
         self._protector_distance, self._protector_move_vector = protector.get_distance_and_move_vector(self.get_current_position)
-    
-    def get_last_position_in_sight(self):
-        return self._last_position_in_sight
                   
     def deduct_next_move(self, protector, treasure):
         
@@ -38,8 +34,7 @@ class Hunter(IntelligentPlayer):
         
         if protector_distance != np.inf:
             protector_move_vector = -1 * protector_move_vector
-            self._last_position_in_sight = self._current_position
-        
+                
         # Deduct weights
         treasure_weight, protector_weight = self.calculate_treasure_based_weights(protector_distance != np.inf, protector_treasure_distance)
         
@@ -58,7 +53,7 @@ class Hunter(IntelligentPlayer):
         self.set_next_move_vector(next_move_vector)
         self.move()
         self.update_status()
-    
+
     def did_you_get_treasure(self, treasure, effective_distance):
         if not self._is_treasure_hunted:
             if VectorUtils.find_distance_between_two_points(self.get_current_position(), treasure.get_current_position()) < effective_distance:

players.py
@@ -7,7 +7,7 @@ class Player:
         self._current_position = kwargs.get('current_position', np.zeros((1,2)))
         self._next_move_vector = kwargs.get('next_move_vector', np.zeros((1,2)))
         self._previous_position = np.copy(self._current_position)
-        self._previous_move_vector = np.zeros((1,2))
+        self._previous_move_vector = np.copy(self._next_move_vector)
         self._velocity_inertia_reduction_formula = kwargs.get('velocity_reduction_inertia_formula', lambda theta: 1)
         
     def get_current_position(self):

protectors.py
@@ -1,6 +1,7 @@
 from intelligent_players import IntelligentPlayer
 from vector_utils import VectorUtils
 import numpy as np
+from copy import copy
 
 
 class Protector(IntelligentPlayer):
@@ -9,6 +10,7 @@ class Protector(IntelligentPlayer):
 
         self._theta_effect = kwargs.get('deviation_effect', lambda theta: (np.pi-theta)/np.pi)
         self._is_alive = True
+        self._hunter_last_position_in_sight = None
         
     def find_vector_deviations(self, unit_vector):
         return [
@@ -50,19 +52,26 @@ class Protector(IntelligentPlayer):
         # Update treasure status (distance and move vector)
         if self._treasure_distance is None:
             self.update_treasure_status(treasure)   
-        
+            
         # Update hunter status
-        if hunter.get_last_position_in_sight() is not None:
-            # hunter.set_current_position(hunter.get_last_position_in_sight())
-            hunter_distance, hunter_treasure_distance, hunter_move_vector = self.find_distance_and_move_vector_to(hunter, treasure)
-        else:
+        hunter_distance, hunter_treasure_distance, hunter_move_vector = self.find_distance_and_move_vector_to(hunter, treasure)
+        
+        # Deduct weights
+        if hunter_distance == np.inf and self._hunter_last_position_in_sight is None:
+            treasure_weight, hunter_weight = self.calculate_treasure_based_weights(False, hunter_treasure_distance)
+            
+        elif hunter_distance == np.inf and self._hunter_last_position_in_sight is not None:
+            hunter.set_current_position(self._hunter_last_position_in_sight)
             hunter_distance, hunter_treasure_distance, hunter_move_vector = self.find_distance_and_move_vector_to(hunter, treasure)
             
-        # Deduct weights
-        if hunter_distance == np.inf:
-            treasure_weight, hunter_weight = self.calculate_treasure_based_weights(False, hunter_treasure_distance)
+            if hunter_move_vector is not None:
+                treasure_weight, hunter_weight = self.calculate_treasure_based_weights(True, hunter_treasure_distance)
+            else:
+                treasure_weight, hunter_weight = self.calculate_treasure_based_weights(False, hunter_treasure_distance)
+                
         else:
             treasure_weight, hunter_weight = self.calculate_treasure_based_weights(True, hunter_treasure_distance)
+            self._hunter_last_position_in_sight = copy(hunter.get_current_position())
         
         # Apply treasure weight to guide vectors
         treasure_weights = self.find_treasure_move_vectors(treasure_weight)

test_controllers.py
@@ -27,23 +27,25 @@ class TestHunters(unittest.TestCase):
         
         cls._hunter = Hunter(
             step_size=0.01,
-            current_position=np.array([0.6, 0.5]).reshape(1, -1),
+            current_position=np.array([0.45, 0.8]).reshape(1, -1),
+            next_move_vector=np.array([1, 0]).reshape(1, -1),
             velocity_reduction_inertia_formula=lambda theta: 1/(1+theta),
-            number_of_vectors=16,
+            number_of_vectors=32,
             map=cls._map,
-            boundaries_instruction=lambda distance: 1 / (1 + np.exp(-500 * (distance - 0.05))),
-            treasure_instruction=lambda relative_distance: np.exp(-0.1 * relative_distance),
+            boundaries_instruction=lambda distance: 1 / (1 + np.exp(max(-100 * (distance - 0.08), -700))),
+            treasure_instruction=lambda relative_distance: np.exp(max(-2 * relative_distance, -700)),
             inertia_instruction = lambda deviation: np.exp(-0.08 * deviation),
         )
         
         cls._protector = Protector(
-            step_size=0.01,
-            current_position=np.array([1.5, 0.2]).reshape(1, -1),
+            step_size=0.011,
+            current_position=np.array([1.03, 0.36]).reshape(1, -1),
+            next_move_vector=np.array([-1, 0]).reshape(1, -1),
             velocity_reduction_inertia_formula=lambda theta: 1/(1+theta),
-            number_of_vectors=16,
+            number_of_vectors=32,
             map=cls._map,
-            boundaries_instruction=lambda distance: 1 / (1 + np.exp(-500 * (distance - 0.05))),
-            treasure_instruction=lambda relative_distance: 0,
+            boundaries_instruction=lambda distance: 1 / (1 + np.exp(max(-100 * (distance - 0.08), -700))),
+            treasure_instruction=lambda relative_distance: np.exp(max(-100 * relative_distance, -700)),
             inertia_instruction = lambda deviation: np.exp(-0.08 * deviation),
         )
         # np.exp(-5000000 * relative_distance)

vector_utils.py
@@ -1,6 +1,7 @@
 from numpy import *
 import numpy as np
-np.seterr(divide='ignore')
+# np.seterr(divide='raise')
+np.seterr(all='raise')
 
 
 class VectorUtils:
@@ -9,13 +10,15 @@ class VectorUtils:
         """
             Returns the unit vector of the vector.  
         """
+        
         if vector is None:
             return vector
         
-        unit_vector = vector / np.linalg.norm(vector)
-            
-        if np.isnan(unit_vector).any():
-            raise ZeroDivisionError
+        try:
+            unit_vector = vector / np.linalg.norm(vector)
+        except:
+            return None
+        
         return unit_vector
     
     @staticmethod
@@ -52,17 +55,62 @@ class VectorUtils:
 
     def find_segment_intersect(a1, a2, b1, b2) :
         
+        # da = a2-a1
+        # db = b2-b1
+        # dp = a1-b1
+        # dap = VectorUtils.perpare_line(da)
+        # denom = dot(dap, db)
+        # num = dot(dap, dp )
+        # intersection = (num / denom.astype(float))*db + b1
+    
         try:
-            da = a2-a1
-            db = b2-b1
-            dp = a1-b1
-            dap = VectorUtils.perpare_line(da)
-            denom = dot( dap, db)
-            num = dot( dap, dp )
-            intersection = (num / denom.astype(float))*db + b1
-            return intersection
-        except Exception as e:
+            m1 = (a2[1] - a1[1]) / (a2[0] - a1[0])
+        except:
+            m1 = np.inf
+        
+        try:
+            m2 = (b2[1] - b1[1]) / (b2[0] - b1[0])
+        except:
+            m2 = np.inf
+        
+        if m1 == np.inf:
+            intercept1 = np.inf
+        else:
+            intercept1 = a1[1] - m1 * a1[0]
+            
+        if m2 == np.inf:
+            intercept2 = np.inf
+        else:
+            intercept2 = b1[1] - m2 * b1[0]
+        
+        if m1 != np.inf and m2 != np.inf:
+            
+            if m1 == m2:
+                return None
+            
+            else:
+                x_intersection = (intercept2 - intercept1) / (m1 - m2)
+                y_intersection = m1 * x_intersection + intercept1
+            
+        elif m1 == np.inf and m2 != np.inf:
+            x_intersection = a1[0]
+            y_intersection = m2 * x_intersection + intercept2
+        
+        elif m1 != np.inf and m2 == np.inf:
+            x_intersection = b1[0]
+            y_intersection = m1 * x_intersection + intercept1
+        
+        elif m1 == np.inf and m2 == np.inf:
             return None
+        
+        intersection = np.array(
+            [
+                x_intersection,
+                y_intersection
+            ]
+        )
+
+        return intersection
     
     @staticmethod
     def find_lines_intersection(line_01, line_02):
@@ -71,6 +119,7 @@ class VectorUtils:
         end_01 = line_01[1].ravel()
         start_02 = line_02[0].ravel()
         end_02 = line_02[1].ravel()
+        
         return VectorUtils.find_segment_intersect(start_01, end_01, start_02, end_02)
     
     @staticmethod
@@ -82,7 +131,9 @@ class VectorUtils:
         line_x_max = max(line_point_01[0], line_point_02[0])
         line_y_min = min(line_point_01[1], line_point_02[1])
         line_y_max = max(line_point_01[1], line_point_02[1])
-        if (line_x_min <= point[0] <= line_x_max) and (line_y_min <= point[1] <= line_y_max):
+        
+        
+        if ((point[0] - line_x_min) >= -0.0000001) and ((line_x_max - point[0]) >= -0.0000001) and ((point[1] - line_y_min) >= -0.0000001) and ((line_y_max - point[1]) >= -0.0000001):
             return True
 
         return False